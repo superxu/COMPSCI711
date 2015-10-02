2bd72a7a13a603fbe8b1ca0e878e4d7a3d86b328
Shupeng Xu <scofield1025@gmail.com>
2015-10-02 21:43:54 +1300

    backup 02/10/2015 21:43

--- A2/ftpserver/filecache/cache.py
+++ A2/ftpserver/filecache/cache.py
@@ -1,23 +1,25 @@
 #!/usr/bin/env python
 # Author: Name: Shupeng Xu  UPI: sxu487   Student ID: 8260026
 
-
 import sys
 import os
 import json
 import time
 import datetime
-import shutil
-
-   
 
 
 def check_cachelog_in_dir(dirname):
-    if not os.path.exists(dirname + "/" + "cache.log"): 
+    if not os.path.exists(dirname + "/" + "fileinfo.log"): 
         return False
 
     return True
 
+
+def check_requestlog_in_dir(dirname):
+    if not os.path.exists(dirname + "/" + "requestinfo.log"): 
+        return False
+
+    return True
 
 
 def same_mtimes(t1, t2):
@@ -30,8 +32,21 @@ def compare_mtime(t1, t2):
 
 
 
+
+def get_requestlog_content(dirname):
+    requestlog = dirname + "/"+ "requestinfo.log"
+    values = {}
+
+    if not os.stat(requestlog).st_size == 0:
+        fd_read =  open(requestlog, "r")
+        values = json.load(fd_read)
+        fd_read.close()
+
+    return values
+
+
 def get_cachelog_content(dirname):
-    cachelog = dirname + "/"+ "cache.log"
+    cachelog = dirname + "/"+ "fileinfo.log"
     values = {}
 
     if not os.stat(cachelog).st_size == 0:
@@ -50,9 +65,9 @@ def is_empty(any_structure):
         return True
 
 
-def write_info_tofile(dirname, new_values):
+def write_cacheinfo_tofile(dirname, new_values):
     newfilelist = os.listdir(dirname)
-    cachelog = dirname + "/"+ "cache.log"
+    cachelog = dirname + "/"+ "fileinfo.log"
 
     # get history values first
     old_values = get_cachelog_content(dirname)
@@ -65,7 +80,35 @@ def write_info_tofile(dirname, new_values):
     else:
         fd_write =  open(cachelog, "w")
         for i in range(0, len(newfilelist)):
-            if newfilelist[i] == "cache.log":   
+            if (newfilelist[i] == "fileinfo.log") or (newfilelist[i] == "requestinfo.log"):   
+                pass
+            else: 
+                if (os.path.isfile(dirname + "/" + newfilelist[i])):
+                    # new file added
+                    old_values[newfilelist[i]] = new_values[newfilelist[i]]
+    
+
+
+        json.dump(old_values, fd_write, indent=8)
+        fd_write.close()
+
+
+def write_requestinfo_tofile(dirname, new_values):
+    newfilelist = os.listdir(dirname)
+    requestlog = dirname + "/"+ "requestinfo.log"
+
+    # get history values first
+    old_values = get_requestlog_content(dirname)
+
+    if (os.stat(requestlog).st_size == 0) or (is_empty(old_values)):
+        fd_write_first =  open(requestlog, "w")
+        json.dump(new_values, fd_write_first, indent=8)
+        fd_write_first.close()
+   
+    else:
+        fd_write =  open(requestlog, "w")
+        for i in range(0, len(newfilelist)):
+            if (newfilelist[i] == "fileinfo.log") or (newfilelist[i] == "requestinfo.log"):   
                 pass
             else: 
                 if (os.path.isfile(dirname + "/" + newfilelist[i])):
@@ -84,7 +127,7 @@ def write_info_tofile(dirname, new_values):
 # add new key/value pair
 # find exisited key and extend it
 def update_cachelog(dirname, key,  new_value):
-    cachelog = dirname + "/"+ "cache.log"
+    cachelog = dirname + "/"+ "fileinfo.log"
 
     # get history values first
     origin_values = get_cachelog_content(dirname)
@@ -113,15 +156,15 @@ def gen_dir_loginfo(dirname):
     if len(filelist) == 0:
         return
 
-    cachelog = dirname + "/"+ "cache.log"
-    sha256_values = {}
+    cachelog = dirname + "/"+ "fileinfo.log"
+    loginfo = {}
 
 
     for i in range(0, len(filelist)):
 
         if (os.path.isfile(dirname + "/" + filelist[i])):
-            # do not create info of cache.log file
-            if filelist[i] == "cache.log":   
+            # do not create info of fileinfo.log file
+            if (filelist[i] == "fileinfo.log") or (filelist[i] == "requestinfo.log"):   
                 pass
             else:  
                 last_modified = time.strftime("%Y-%m-%d %H:%M:%S %z", time.localtime(os.path.getmtime(dirname + "/"+ filelist[i])))
@@ -129,29 +172,136 @@ def gen_dir_loginfo(dirname):
                 valuelist = []
                 valuelist.append(last_modified)
 
-                sha256_values.setdefault(filelist[i], []).append(valuelist)
+                loginfo.setdefault(filelist[i], []).append(valuelist)
              
         # do not process directories
         else:
             pass
 
-    # write SHA256 value to cache.log file
-    write_info_tofile(dirname, sha256_values)
+    # write SHA256 value to fileinfo.log file
+    write_cacheinfo_tofile(dirname, loginfo)
 
+
+
+
+def gen_dir_requestinfo(dirname):
+
+    filelist = os.listdir(dirname)
+
+    if len(filelist) == 0:
+        return
+
+    requestlog = dirname + "/"+ "requestinfo.log"
+    requestinfo = {}
+
+
+    for i in range(0, len(filelist)):
+
+        if (os.path.isfile(dirname + "/" + filelist[i])):
+            # do not create info of fileinfo.log file
+            if (filelist[i] == "fileinfo.log") or (filelist[i] == "requestinfo.log"):   
+                pass
+            else:  
+                valuelist = []
+                requestinfo.setdefault(filelist[i], []).append(valuelist)
+             
+        # do not process directories
+        else:
+            pass
+
+    # write info to requestinfo.log file
+    write_requestinfo_tofile(dirname, requestinfo)
 
 
 def create_cachelog(dirname):
     if not check_cachelog_in_dir(dirname):
-    	os.system("touch cache.log")
+    	os.system("touch fileinfo.log")
   
+
+def create_requestlog(dirname):
+    if not check_requestlog_in_dir(dirname):
+        os.system("touch requestinfo.log")
+
+
+def update_requestlog(dirname, filename,  hit):
+   
+    requestlog = dirname + "/"+ "requestinfo.log"
+    origin_values = get_requestlog_content(dirname)
+
+    print("hit = %s" % hit)
+
+    request_time = time.strftime("%Y-%m-%d %H:%M:%S %z", time.localtime())
+    requestinfo = {}
+
+    user_request = "user request:" + " file "  + filename + " at " + request_time
+    if hit == True:
+        response = "response:" + " cached file " + filename
+    else:
+        response = "response:" + " file " + filename  + " downloaded from the server"
+
+    valuelist = []
+    valuelist.append(user_request)
+    valuelist.append(response)
+
+    if origin_values.has_key(filename):
+        fd_write =  open(requestlog, "w")
+        origin_values[filename].extend([valuelist]) 
+        # it seems nothing wrong with reverse or sort. even I comment reverse() below, the position/order of keys may still change
+        origin_values[filename].reverse()     
+        json.dump(origin_values, fd_write, indent=8)
+        fd_write.close()
+    else:
+        requestinfo.setdefault(filename, []).append(valuelist)
+        print("requestinfo = %s" % requestinfo)
+        fd_write_first =  open(requestlog, "w")
+        origin_values[filename] = requestinfo[filename]
+        print("origin_values = %s" % origin_values)
+        json.dump(origin_values, fd_write_first, indent=8)
+        fd_write_first.close()
+
+
+
+def downloadfilefromserver(filename):
+    pass
+
+
+# find out if the file requested is in the directory(cache) and response.
+def process_request(dirname, filename):
+
+    cachelog = dirname + "/" + "fileinfo.log"
+    # get log info values 
+    loginfo = get_cachelog_content(dirname)
+
+
+    if not is_empty(loginfo):
+        # file exists
+        if loginfo.has_key(filename):
+            # update request log
+            update_requestlog(dirname, filename, True)
+        # file does not exist
+        else:
+            downloadfilefromserver(filename)
+            # update request log
+            update_requestlog(dirname, filename, False)
+            pass
+
+    # no files in the cache
+    else:
+        print("Empty cache....")
+        pass
+
 
 
 def main():
 
-    create_cachelog(".")
-    # generate SHA256 of files in the directory
-    gen_dir_loginfo(".")
+    dirname = "."
+    create_cachelog(dirname)
+    create_requestlog(dirname)
+    # generate log of files in the directory
+    gen_dir_loginfo(dirname)
+    gen_dir_requestinfo(dirname)
 
+    process_request(dirname, sys.argv[1])
     
 
 
